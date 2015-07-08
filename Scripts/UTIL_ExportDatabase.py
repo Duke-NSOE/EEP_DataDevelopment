e09084498516bba395c89a50040718a62e91a127
johnpfay <john.fay@duke.edu>
2015-07-08 10:56:01 -0400

    Added script to export SDE databases to XML

---
+++ Scripts/UTIL_ExportDatabase.py
@@ -0,0 +1,24 @@
+#Util_ExportDatabase.py
+#
+# Description: Exports the DMS SDE Databases to an XML document
+#
+# July 2015
+# John.Fay@duke.edu
+
+import sys, os, arcpy, time
+
+#Script variables
+rootPath = os.path.abspath(os.path.join(sys.path[-1],".."))
+dataPath = os.path.join(rootPath,"Data")
+ncSDE = os.path.join(dataPath,"NC.sde")
+nhdSDE = os.path.join(dataPath,"NHD data.sde")
+nlcdSDE = os.path.join(dataPath,"NLCD data.sde")
+
+xmlPath = "C:/temp/XMLs"
+ncXML = os.path.join(xmlPath,"NC.xml")
+nhdXML = os.path.join(xmlPath,"NHD.xml")
+#ncXML = os.path.join(xmlPath,"NC.zip")
+
+print "Converting NC SDE to (zipped) XML"
+arcpy.ExportXMLWorkspaceDocument_management(nhdSDE, nhdXML,"DATA","BINARY","METADATA")
+
